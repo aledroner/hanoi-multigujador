9cccfbafe4f70d89f299ba7d5b00e89f30f4547e
aledroner <aledroner@gmail.com>
2017-06-08 21:41:32 +0200

    Código más limpio del main.js

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -3,31 +3,49 @@ angular
 	.component('app', {
 		templateUrl: 'app/main.html',
 		controller: function($stateParams, $state, $timeout, toastr) {
-			const vm = this;
-			const refUsers = getRef('users');
-			const refGames = getRef('games');
-			vm.userLogged = {
+
+			// Constantes
+			const VM = this;
+			const REF_USERS = getRef('users');
+			const REF_GAMES = getRef('games');
+
+			// Mensajes
+			const MSG_SIGNIN = '¡Saludos! ';
+			const MSG_SIGNOUT = '¡Hasta pronto figura!';
+			const MSG_ERROR = 'Ha ocurrido un error escandaloso.';
+			const MSG_GAME_ACTIVED = 'Ya estás en una partida, no pretendas ser omnipresente.';
+			const MSG_GAME_CREATED = 'Partida creada. Ve a por un snack mientras esperas a tu oponente.';
+			const MSG_GAME_JOINED = '¿¡Osas retar a ';
+			const MSG_GAME_DELETED = 'Partida directa al incinerador.';
+
+			// Usuario logueado
+			VM.userLogged = {
 				profile: {
 					picture: 'app/img/user.png'
 				}
 			}
 
-			// Variable que controla los stateParams
+			/**
+			 * Timeout para establece los stateParams
+			 */
 			$timeout(function() {
-				vm.stateParams = $stateParams;
+				VM.stateParams = $stateParams;
 			});
 
-			// Actualiza el array de partidas online
-			refGames.on('value', function(snap) {
+			// Evento que actualiza el array de partidas online
+			REF_GAMES.on('value', function(snap) {
 				$timeout(function() {
-					vm.gamesOnline = $.map(snap.val(), function(value, index) {
+					VM.gamesOnline = $.map(snap.val(), function(value, index) {
 						return [value];
 					});
 				});
 			});
 
-			// Inicia sesión
-			vm.signIn = function(social) {
+			/**
+			 * Inicia sesión
+			 * @param  {String} social Red social con la que inicias sesión
+			 */
+			VM.signIn = function(social) {
 				var provider;
 				if (social === 't') provider = new firebase.auth.TwitterAuthProvider();
 				if (social === 'g') provider = new firebase.auth.GoogleAuthProvider();
@@ -35,56 +53,62 @@ angular
 					.then(function(result) {
 						var user = createUser(social, result);
 						var uid = result.user.uid;
-						refUsers.child(uid).set(user)
+						REF_USERS.child(uid).set(user)
 						localStorage.setItem('userId', uid);
-						toastr.success('¡Saludos! ' + user.profile.name);
+						toastr.success(MSG_SIGNIN + user.profile.name);
 					})
 					.catch(function(error) {
 						toastr.error(error.message);
 					});
 			}
 
-			// Cierra sesión
-			vm.signOut = function() {
+			/**
+			 * Cierra sesión
+			 */
+			VM.signOut = function() {
 				firebase.auth().signOut()
 					.then(function() {
 						go(null);
 						localStorage.setItem('userId', '');
-						toastr.info('¡Hasta pronto figura!');
+						toastr.info(MSG_SIGNOUT);
 					}).catch(function(err) {
-						toastr.success('Ha ocurrido un error escandaloso.')
+						toastr.success(MSG_ERROR)
 					});
 			}
 
-			// Crea o se une a una partida nueva multijugador
-			vm.crearPartida = function(gameId, mode) {
+			/**
+			 * Crea o se une a una partida nueva multijugador
+			 * @param  {String} gameId Id aleatoria del juego
+			 * @param  {String} mode   Modo de entrar ['Crear' | 'Unir' | 'Ver']
+			 */
+			VM.crearPartida = function(gameId, mode) {
 				var userId = localStorage.getItem('userId');
 				if (mode == 'crear') {
-					refUsers.child(userId).once('value', function(snap) {
+					REF_USERS.child(userId).once('value', function(snap) {
 						var user = snap.val();
 						if (user.activeGame) {
-							toastr.info('Ya estás en una partida, no pretendas ser omnipresente.');
+							toastr.info(MSG_GAME_ACTIVED);
 						} else {
 							go(gameId);
-							refUsers.child(userId).update({
+							REF_USERS.child(userId).update({
 								activeGame: true
 							});
 							setGame(userId, gameId, mode);
-							toastr.success('Partida creada. Ve a por un snack mientras esperas a tu oponente.');
+							toastr.success(MSG_GAME_CREATED);
 						}
 					});
 				} else if (mode == 'unir') {
-					refGames.child(gameId).once('value', function(snap) {
+					REF_GAMES.child(gameId).once('value', function(snap) {
 						var game = snap.val();
 						// if (user.activeGame) {
 						// 	toastr.info('Ya estás en una partida, no pretendas ser omnipresente.');
 						go(gameId);
 						if (game.player1.uid != userId) {
-							refUsers.child(userId).update({
+							REF_USERS.child(userId).update({
 								activeGame: true
 							});
 							setGame(userId, gameId, mode);
-							toastr.success('¿¡Osas retar a ' + game.player1.name + '!?');
+							toastr.success(MSG_GAME_JOINED + game.player1.name + '!?');
 						}
 					})
 				} else if (mode == 'ver') {
@@ -92,28 +116,33 @@ angular
 				}
 			}
 
-			// Crea o se une a una partida nueva multijugador
-			vm.borrarPartida = function(gameId, player1Id, player2Id) {
+			/**
+			 * Se une a una partida ya creada
+			 * @param  {String} gameId    Id aleatoria del juego
+			 * @param  {String} player1Id Id aleatoria del jugador 1
+			 * @param  {String} player2Id Id aleatoria del jugador 2
+			 */
+			VM.borrarPartida = function(gameId, player1Id, player2Id) {
 				if (player1Id) {
-					refUsers.child(player1Id).update({
+					REF_USERS.child(player1Id).update({
 						activeGame: false
 					});
 				}
 				if (player2Id) {
-					refUsers.child(player2Id).update({
+					REF_USERS.child(player2Id).update({
 						activeGame: false
 					});
 				}
-				refGames.child(gameId).remove();
-				toastr.info('Partida directa al incinerador.');
+				REF_GAMES.child(gameId).remove();
+				toastr.info(MSG_GAME_DELETED);
 			}
 
 			// Evento cuando hay un cambio en la autenticación
 			firebase.auth().onAuthStateChanged(function(user) {
 				if (user) {
 					$stateParams.log = true;
-					refUsers.child(localStorage.getItem('userId')).on('value', function(snap) {
-						vm.userLogged = snap.val();
+					REF_USERS.child(localStorage.getItem('userId')).on('value', function(snap) {
+						VM.userLogged = snap.val();
 					})
 				} else {
 					$stateParams.log = false;
@@ -121,32 +150,38 @@ angular
 				}
 			});
 
-			// Setea quién es el jugador uno y el jugador dos
+			// Setea quién es el jugador 1 y el jugador 2
 			if ($stateParams.game != null) {
-				refGames.child($stateParams.game).on('value', function(snap) {
+				REF_GAMES.child($stateParams.game).on('value', function(snap) {
 					$timeout(function() {
 						var game = snap.val();
 						var userId = localStorage.getItem('userId')
 						if (game.player1.uid === userId) {
-							vm.player1 = snap.val().player1;
-							vm.player2 = snap.val().player2;
+							VM.player1 = snap.val().player1;
+							VM.player2 = snap.val().player2;
 						} else {
-							vm.player1 = snap.val().player2;
-							vm.player2 = snap.val().player1;
+							VM.player1 = snap.val().player2;
+							VM.player2 = snap.val().player1;
 						}
 					});
 				});
 			}
 
-			// Te lleva a una partida
+			/**
+			 * Te lleva a una partida
+			 * @param  {String} gameId Id aleatoria del juego
+			 */
 			function go(gameId) {
 				$state.go($state.current, {
 					game: gameId
 				});
 			}
 
-			// Genera un id aleatorio para un juego nuevo
-			vm.generarIdGame = function() {
+			/**
+			 * Genera un id aleatorio para un juego nuevo
+			 * @return {String} Cadena alfanumérica aleatorioa
+			 */
+			VM.generarIdGame = function() {
 				return randomId();
 			}
 		}
