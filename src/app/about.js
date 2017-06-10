ff8ce2ee4d8723268bccfdd51e193115f36fb9c1
aledroner <aledroner@gmail.com>
2017-06-11 01:21:09 +0200

    Acerca de terminado y boton accion table games colocado bien

--- src/app/about.js
+++ src/app/about.js
@@ -2,4 +2,12 @@ angular
 	.module('hanoi.about', [])
 	.component('hanoiAbout', {
 		templateUrl: 'app/about.html'
+	})
+	.controller('techCtrl', function($http) {
+		const vm = this;
+		$http
+			.get('app/techs.json')
+			.then(function(response) {
+				vm.techs = response.data;
+			});
 	});
