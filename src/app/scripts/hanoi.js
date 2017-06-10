730ad3f1f42dfefd8949a30a400cac33e956ddaf
aledroner <aledroner@gmail.com>
2017-06-10 11:33:21 +0200

    Base de madera creada en los dos jugadores

--- src/app/scripts/hanoi.js
+++ src/app/scripts/hanoi.js
@@ -1,8 +1,10 @@
 angular
 	.module('hanoi.canvas', [])
 	.controller('CanvasCtrl', function CanvasCtrl($scope) {
-		var canvas = document.getElementById('player1-canvas');
-		var ctx = canvas.getContext('2d');
+		var canvas1 = document.getElementById('player1-canvas');
+		var canvas2 = document.getElementById('player2-canvas');
+		var ctx1 = canvas1.getContext('2d');
+		var ctx2 = canvas2.getContext('2d');
 
 		// Colores discos
 		var colors = ['#d63d3d', '#d6743d', '#d3d63d', '#3dd663', '#3dd6d6', '#3d63d6', '#653dd6', '#a13dd6', '#d63d90'];
@@ -10,8 +12,8 @@ angular
 		var borderColor = '#222';
 
 		// Tamaño canvas. También evita pixelado
-		var w = canvas.width = getComputedStyle(canvas).width.split('px')[0];
-		var h = canvas.height = getComputedStyle(canvas).height.split('px')[0];
+		var w = canvas1.width = canvas2.width = getComputedStyle(canvas1).width.split('px')[0];
+		var h = canvas1.height = canvas2.height = getComputedStyle(canvas1).height.split('px')[0];
 
 		// Tamaños
 		var tam = 16;
@@ -23,25 +25,23 @@ angular
 		/**
 		 * Dibuja la base de madera donde se colocarán los discos
 		 */
-		function init() {
+		function init(context) {
 			// Base madera
-			ctx.fillStyle = borderColor;
-			ctx.fillRect(0, baseY, w, tam);
-			ctx.fill();
-			ctx.fillStyle = woodColor;
-			ctx.fillRect(0 + border, baseY + border, w - border * 2, tam - border * 2);
+			context.fillStyle = borderColor;
+			context.fillRect(0, baseY, w, tam);
+			context.fill();
+			context.fillStyle = woodColor;
+			context.fillRect(0 + border, baseY + border, w - border * 2, tam - border * 2);
 
 			// Palos madera
 			for (var i = -1; i <= 1; i++) {
-				ctx.fillStyle = borderColor;
-				ctx.fillRect((gapX * (i + 2) + (tam / 2) * i * 4) - (tam / 2), topY, tam, baseY - topY);
-				ctx.fillStyle = woodColor;
-				ctx.fillRect((gapX * (i + 2) + (tam / 2) * i * 4) - (tam / 2) + border, topY + border, tam - border * 2, baseY - topY - border);
+				context.fillStyle = borderColor;
+				context.fillRect((gapX * (i + 2) + (tam / 2) * i * 4) - (tam / 2), topY, tam, baseY - topY);
+				context.fillStyle = woodColor;
+				context.fillRect((gapX * (i + 2) + (tam / 2) * i * 4) - (tam / 2) + border, topY + border, tam - border * 2, baseY - topY - border);
 			}
-
-			ctx.fill();
-
+			context.fill();
 		}
-		init();
-
+		init(ctx1);
+		init(ctx2);
 	});;
